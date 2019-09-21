039ce4b13a89121974383fc1ef777216bca39d6a
Zack Claar <claar.zack@gmail.com>
2019-09-20 19:17:48 -0600

    Simple NextJS appliation created. Time to learn more

---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import { renderToHTML } from "next/dist/next-server/server/render"
+
+const Index = () => {
+        <div>
+            <p>
+                Hello, world and Next.js!
+            </p>
+        </div>
+};
+
+export default Index;
